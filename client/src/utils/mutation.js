e609dc4ca833132a9712d989a534aa24516cbb73
Nick Garza <thenickgarza14@gmail.com>
2022-04-14 20:03:36 -0500

    aded the login user mutation

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -85,3 +85,15 @@ export const ADD_OFFER = gql`
     }
   }
 `;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      User {
+        _id
+        name
+      }
+    }
+  }
+`;
