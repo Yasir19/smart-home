a50f4db3eadf7ff0a15e03dbc6a4374540174d8a
Kyro <mariogallardo1331@yahoo.com>
2022-04-15 14:36:26 -0500

    creating project

---
+++ client/src/components/CreateProject/CreateProject.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const createProject = () => {
+    return (
+        <section>
+            <div className="project-init">
+                <p>Customer Name: </p>
+                <p>Project Description: </p>
+                <p>Project Zip Code: </p>
+                <p>Project Materials: </p>
+                <p>Initial Price: </p>
+            </div>
+        </section>
+    )
+}
+
+export default createProject;
