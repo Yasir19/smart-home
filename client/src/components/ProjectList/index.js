2251edd7bf2c34958c169293b03bd6c5acba1801
yasir19 <yasir.habboo@gmail.com>
2022-04-17 00:17:41 -0500

    fixing project list

--- client/src/components/ProjectList/index.js
+++ client/src/components/ProjectList/index.js
@@ -14,7 +14,7 @@ const ProjectList = ({ projects, projectDescription }) => {
           <div key={project._id} className="card mb-3">
             <div className="card-body">
               <Link to={`project/${project._id}`}>
-                <p className="mb-0">Project Owner:{project.yourName}</p>
+                <p className="mb-0">Project Owner:{project.userName}</p>
                 <p>Project Description:{project.projectDescription}</p>
                 <p className="mb-0">
                   Project Materials:{project.projectMaterials}
