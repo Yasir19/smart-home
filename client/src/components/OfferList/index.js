0bb779a50cab769dfd05475cc6385c7eaaf22dd2
yohans14 <yohans14@yahoo.com>
2022-04-18 20:24:58 -0500

    Nav bar fixed and offer set value

--- client/src/components/OfferList/index.js
+++ client/src/components/OfferList/index.js
@@ -3,49 +3,48 @@ import { useMutation } from "@apollo/client";
 import { APPROVE_OFFER } from "../../utils/mutation";
 
 const OfferList = ({ offers, projectId }) => {
-  const [isOffer, setIsOffer] = useState({newOffer:""});
-  const [approveOffer, { error }] = useMutation(APPROVE_OFFER);
+	const [newOffer,setIsOffer] = useState("");
+	const [approveOffer, { error }] = useMutation(APPROVE_OFFER);
 
-  const handleChange = (e) => {
-    const {name,value} = e.target
-    setIsOffer({
-      ...isOffer,
-      [name]:value,
-    })
-  };
-  console.log(isOffer)
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      await approveOffer({
-        variables: { isOffer, projectId },
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  if (!offers.length) {
-    return <h3>No Offer Yet</h3>;
-  }
-  return (
-    <div>
-      {offers &&
-        offers.map((offer) => (
-          <div className="card mb-3" key={offer.newOffer}>
-            <div className="pill mb-3">
-              <p className="card-body"> Offer By:{offer.ContractorName}</p>
-              <form
-                className="flex-row justify-center justify-space-between-md align-stretch"
-                onSubmit={handleFormSubmit}
-              >
-                <button name={offer.newOffer+1} value={offer.newOffer} onChange={handleChange}>
-                  The Offer Value:{offer.newOffer}{" "}
-                </button>
-              </form>
-            </div>
-          </div>
-        ))}
-    </div>
-  );
+	const handleChange = (e) => {
+		setIsOffer (e.target.value)
+	};
+	const handleFormSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			await approveOffer({
+				variables: { newOffer, projectId  },
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	};
+	if (!offers.length) {
+		return <h3>No Offer Yet</h3>;
+	}
+	return (
+		<div>
+			{offers &&
+				offers.map((offer) => (
+					<div className="card mb-3" key={offer.newOffer}>
+						<div className="pill mb-3">
+							<p className="card-body"> Offer By:{offer.ContractorName}</p>
+							<form
+								className="flex-row justify-center justify-space-between-md align-stretch"
+								onSubmit={(e) => handleFormSubmit (e, offer.newOffer)}
+							>
+								<button
+									name={offer.ContractorName}
+									value={offer.newOffer}
+									onClick={handleChange}
+								>
+									The Offer Value:{offer.newOffer}{" "}
+								</button>
+							</form>
+						</div>
+					</div>
+				))}
+		</div>
+	);
 };
 export default OfferList;
