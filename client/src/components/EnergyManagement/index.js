e7dfd5c49c0e2b374041d82149f5015d246d6272
escowin <edwin@escowinart.com>
2022-04-20 16:40:11 -0500

    text styles & reformats

--- client/src/components/EnergyManagement/index.js
+++ client/src/components/EnergyManagement/index.js
@@ -7,12 +7,17 @@ const EnergyManagement = () => {
             <article className="card-container">
                 <div className="card-approved-list" style={{ width: "18rem" }}>
                     <h4 className="card-title">Energy Management</h4>
-                    <img src="https://img.freepik.com/free-photo/digital-tablet-screen-with-smart-home-controller-wooden-table_53876-96317.jpg?t=st=1650333133~exp=1650333733~hmac=e3f7a4c08e987d974e39d55a48e821a7f247b49d7e8c401415456988799e2d29&w=740" alt="Smart home system" width={300}></img>
+                    <img className="img" src="https://img.freepik.com/free-photo/digital-tablet-screen-with-smart-home-controller-wooden-table_53876-96317.jpg?t=st=1650333133~exp=1650333733~hmac=e3f7a4c08e987d974e39d55a48e821a7f247b49d7e8c401415456988799e2d29&w=740" alt="Smart home system" width={300}></img>
                     <p>
                         Here at Smart Home Enterprises our goal is to make an everlasting change
-                        on the environment and on our future!  We offer services in energy management from
+                        on the environment and on our future!
+                        <br/> <br/> 
+                         We offer services in energy management from
                         the smallest upgrades such as installing smart lights, smart plugs, etc. 
-                        To major projects such as Solar Panel installation.  Reach out to a contractor today
+                        To major projects such as Solar Panel installation. 
+                        <br/> 
+                        <br/> 
+                        Reach out to a contractor today
                         if you are tired of your pricey monthly electricty costs and let us guide you into the
                         future!
                     </p>
--- client/src/components/HomeSecurity/index.js
+++ client/src/components/HomeSecurity/index.js
@@ -6,12 +6,16 @@ const HomeSecurity = () => {
             <article className="card-container">
                 <div className="card-approved-list" style={{ width: "18rem" }}>
                     <h4 className="card-title">Home Security</h4>
-                    <img src="https://img.freepik.com/free-vector/isometric-smart-home-technology-interface-smartphone-app-screen-with-augmented-reality-ar-view_122058-956.jpg?w=740" alt="Home Security" width={300}></img>
+                    <img className="img" src="https://img.freepik.com/free-vector/isometric-smart-home-technology-interface-smartphone-app-screen-with-augmented-reality-ar-view_122058-956.jpg?w=740" alt="Home Security" width={300}></img>
                     <p>
-                        Who wants to run more wires through your home? If your answer was,
+                        Who wants to run more wires through your home?
+                        <br/> <br/> 
+                        If your answer was,
                         "Oh please no", then you are at the right place!  Here at Smart Home
-                        Enterprises we offer a varity of smart home security systems to meet
-                        any need you may have to protect you and your loved ones! To set up a
+                        Enterprises we offer a variety of smart home security systems to meet
+                        any need you may have to protect you and your loved ones! 
+                        <br/> <br/> 
+                        To set up a
                         consulation today reach out to our team at 1-800-SMART-HOME.
                     </p>
                 </div>
