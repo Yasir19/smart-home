c0573d88af5381880daefcc4e76a4ddb653267e3
Kyro <mariogallardo1331@yahoo.com>
2022-04-19 20:41:59 -0500

    wrapping up Life Style Support component

--- client/src/components/LifeStyleSupport/index.js
+++ client/src/components/LifeStyleSupport/index.js
@@ -5,14 +5,17 @@ const LifeStyleSupport = () => {
         <section className="approved-list my-5">
             <article className="card-container">
                 <div className="card-approved-list" style={{ width: "18rem" }}>
-                    <h4 className="card-title">Home Security</h4>
-                    <img src="https://img.freepik.com/free-vector/internet-things-concept_1284-10812.jpg?t=st=1650336184~exp=1650336784~hmac=88d5fe240c8d0e8dde4a30c8863818e7c1cae29c2ba8b25813c2bea60fff4fc3&w=996" alt="Home Security" width={300}></img>
+                    <h4 className="card-title">LifeStyle Support</h4>
+                    <img src="https://img.freepik.com/free-vector/internet-things-concept_1284-10812.jpg?t=st=1650336184~exp=1650336784~hmac=88d5fe240c8d0e8dde4a30c8863818e7c1cae29c2ba8b25813c2bea60fff4fc3&w=996" alt="Smartphone smart apps" width={300} height={235}></img>
                     <p>
-                        Who wants to run more wires through your home? If your answer was,
-                        "Oh please no", then you are at the right place!  Here at Smart Home
-                        Enterprises we offer a varity of smart home security systems to meet
-                        any need you may have to protect you and your loved ones! To set up a
-                        consulation today reach out to our team at 1-800-SMART-HOME.
+                      Welcome to our LifeStyle Support page!  On this page you will
+                      learn about the various smart home technologies that we can build
+                      into your day to day life to make your life to make it run a little
+                      bit smoother!  Examples of popular smart home services that we offer
+                      here include Phillips Light Systems, Smart Thermostats, Sonos Wireless
+                      Speaker Systems, and countless others! Our LifeStyle Support team stays
+                      up to date with the latest and greatest technologies!  Reach out to our
+                      customer services team to schedule your consultation today at 1-800-SMART-HOME.
                     </p>
                 </div>
             </article>
