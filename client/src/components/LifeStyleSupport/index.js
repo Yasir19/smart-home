a935ce26d0822021ec129aae1cdc1211a30b92c2
Kyro <mariogallardo1331@yahoo.com>
2022-04-19 20:18:53 -0500

    wrapping up Home Security component

--- client/src/components/LifeStyleSupport/index.js
+++ client/src/components/LifeStyleSupport/index.js
@@ -2,8 +2,20 @@ import React from "react";
 
 const LifeStyleSupport = () => {
     return (
-        <section className="">
-            <p>Test</p>
+        <section className="approved-list my-5">
+            <article className="card-container">
+                <div className="card-approved-list" style={{ width: "18rem" }}>
+                    <h4 className="card-title">Home Security</h4>
+                    <img src="https://img.freepik.com/free-vector/internet-things-concept_1284-10812.jpg?t=st=1650336184~exp=1650336784~hmac=88d5fe240c8d0e8dde4a30c8863818e7c1cae29c2ba8b25813c2bea60fff4fc3&w=996" alt="Home Security" width={300}></img>
+                    <p>
+                        Who wants to run more wires through your home? If your answer was,
+                        "Oh please no", then you are at the right place!  Here at Smart Home
+                        Enterprises we offer a varity of smart home security systems to meet
+                        any need you may have to protect you and your loved ones! To set up a
+                        consulation today reach out to our team at 1-800-SMART-HOME.
+                    </p>
+                </div>
+            </article>
         </section>
     );
 };
